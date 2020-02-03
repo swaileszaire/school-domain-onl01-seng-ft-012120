7d3f053a2bbc642410be6f098fd9b35e18f64c3b
Zaire  Swailes <swaileszaire@gmail.com>
2020-02-03 16:27:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School 
+  def initialize(name)
+    @name = name 
+  end
+end
+  
+def student.roster
+  hash = [grade 9] = []
+  hash = [grade 10] = []
+  hash = [grade 11] = []
+end
+
+def add_student (name, grade)
+  school.add_student("Zach Morris", 9)
+  school.add_student("Kelly Kapowski", 10)
+  school.add_student("Screech", 11)
+
+def grade (num)
+
+def grade(num)
+  school.sort_by do |grade|
+  name = "Zach Morris", grade = 9 
+  name = "AC Slater", grade = 9
+  name = "Kelly Kapowski", grade = 10
+  name = "Screech", grade = 11
+end
+end
+
